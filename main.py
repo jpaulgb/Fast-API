c784191cf5958956eed737b30e7ad164f32329f9
jpaulgb <jepgb@hotmail.com>
2025-06-30 21:20:49 -0500

    API Buils with Docs

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI #importamos
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI() #instanciamos
 
-#http://127.0.0.1:8000/
+class Libro(BaseModel):
+    titulo: str
+    autor: str
+    paginas: int
+    editorial: Optional[str] 
 
+#rutas:
 @app.get("/") #creamos ruta raiz
 def index(): 
     return {"message" : "Hola, Fast API. Esto es una funcion tipo Get"}
@@ -14,6 +21,12 @@ def index():
 def mostrar_libro(id: int):
     return {"data": id}
 
+@app.post("/libros")
+def insertar_libro(libro: Libro):
+    return {"message": f"libro {libro.titulo} insertado"}
+
 # levanta el servidor: uvicorn main:app --reload
 # Entornos virtuales: https://docs.python.org/es/3/tutorial/venv.html
 # Installation: https://fastapi.tiangolo.com/#installation
+
+# Documentacion en: http://127.0.0.1:8000/docs
